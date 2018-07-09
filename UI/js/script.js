3369d96f434ad51478e87ce80fc613f840965945
Ijebusoma <vanessaosuka@gmail.com>
2018-07-09 22:33:00 +0100

    fixes modal popup [fixes #158897105]

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,24 +1,13 @@
-var signupModal = document.getElementById('signup-modal');
-var signupButton = document.getElementById('sign-up');
 
-var signinModal = document.getElementById('signin-modal');
+var signinModal = document.getElementById('signin');
 var signinButton = document.getElementById('sign-in');
-
-var span1 = document.getElementsByClassName("close-signup"); /**  Get the <span> element that closes the modal  */
-var span2 = document.getElementsByClassName("close-signin");
-
-signupButton.addEventListener('click',function(){
-    signupModal.style.display='block';
-});
+var span = document.getElementsByClassName("close")[0];  /**  Get the <span> element that closes the modal  */
 
 signinButton.addEventListener('click', function(){
     signinModal.style.display = 'block';
 });
-
-span1.onclick = function() {
-    signupModal.style.display = 'none';
-}
-span2.onclick = function() {
+span.onclick = function() {
     signinModal.style.display = 'none';
 }
+ 
 
