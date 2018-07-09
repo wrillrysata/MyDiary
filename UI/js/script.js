eeb7ad54b6fb4a68928065f6ec712f0d4d988dc5
Ijebusoma <vanessaosuka@gmail.com>
2018-07-09 22:54:20 +0100

    build signup form

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,6 +1,7 @@
 
 var signinModal = document.getElementById('signin');
 var signinButton = document.getElementById('sign-in');
+var signupButton = document.getElementById('sign-up');
 var span = document.getElementsByClassName("close")[0];  /**  Get the <span> element that closes the modal  */
 
 signinButton.addEventListener('click', function(){
@@ -9,5 +10,8 @@ signinButton.addEventListener('click', function(){
 span.onclick = function() {
     signinModal.style.display = 'none';
 }
+signupButton.addEventListener('click', function(){
+window.location.assign("signup.html");
+});
  
 
