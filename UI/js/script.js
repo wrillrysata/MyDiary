479086a6d2c9c89c69496fc7161f7c52ff865211
Ijebusoma <vanessaosuka@gmail.com>
2018-07-10 20:20:26 +0100

    minor adjustments to code

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,17 +1,30 @@
 
+
+function onReady(callback) {
+    /** Checks if the webpage has loaded fully before proceeding to run the app **/
+    document.readyState === "interactive" || document.readyState === "complete"
+      ? callback()
+      : document.addEventListener("DOMContentLoaded", callback);
+  }
+function runApp(){
 var signinModal = document.getElementById('signin');
 var signinButton = document.getElementById('sign-in');
 var signupButton = document.getElementById('sign-up');
-var span = document.getElementsByClassName("close")[0];  /**  Get the <span> element that closes the modal  */
+var span = document.getElementsByClassName('close')[0];  /**  Get the <span> element that closes the modal  */
 
-signinButton.addEventListener('click', function(){
-    signinModal.style.display = 'block';
+
+signinButton.addEventListener('click', () => {
+   signinModal.style.display = 'block';
 });
-span.onclick = function() {
-    signinModal.style.display = 'none';
+span.onclick = () => {
+   signinModal.style.display = 'none';
 }
-signupButton.addEventListener('click', function(){
+
+signupButton.addEventListener('click', () => {
 window.location.assign("signup.html");
 });
- 
+
+
+}
+onReady(runApp);
 
