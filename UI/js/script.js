32a130cedcebe9537266c89f9c35cc2d531f4b9a
Ijebusoma <vanessaosuka@gmail.com>
2018-07-12 23:23:28 +0100

    tidy main script file

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,5 +1,4 @@
 
-
 function onReady(callback) {
     /** Checks if the webpage has loaded fully before proceeding to run the app **/
     document.readyState === "interactive" || document.readyState === "complete"
@@ -7,24 +6,18 @@ function onReady(callback) {
       : document.addEventListener("DOMContentLoaded", callback);
   }
 function runApp(){
-var signinModal = document.getElementById('signin');
 var signinButton = document.getElementById('sign-in');
 var signupButton = document.getElementById('sign-up');
-var span = document.getElementsByClassName('close')[0];  /**  Get the <span> element that closes the modal  */
 
 
 signinButton.addEventListener('click', () => {
-   signinModal.style.display = 'block';
+  window.location.assign("signin.html");
 });
-span.onclick = () => {
-   signinModal.style.display = 'none';
-}
 
 signupButton.addEventListener('click', () => {
 window.location.assign("signup.html");
 });
-
-
 }
+
 onReady(runApp);
 
