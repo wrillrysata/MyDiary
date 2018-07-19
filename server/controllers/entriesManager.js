d3d5d3ef579b4d727a35da08ec25b85488132d48
Ijebusoma <vanessaosuka@gmail.com>
2018-07-19 21:45:53 +0100

    implements modify-entry endpoint

--- server/controllers/entriesManager.js
+++ server/controllers/entriesManager.js
@@ -1,7 +1,7 @@
 const entriesManager = {
   entries: [
     { id: 1, note: 'Lorem ipsum dit' },
-    { id: 2, note: 'Lorem ipsum dit' },
+    { id: 2, note: 'Lorem ipsum dita' },
 
   ],
   getAll: () => entriesManager.entries,
@@ -9,7 +9,7 @@ const entriesManager = {
 
   getOne: (id) => {
     const allEntries = entriesManager.getAll();
-    for (let i = 0; i < allEntries.length; i + 1) {
+    for (let i = 1; i < allEntries.length; i + 1) {
       if (allEntries[i].id === id) {
         return allEntries[i];
       }
@@ -19,7 +19,14 @@ const entriesManager = {
 
   addNew: (id, note) => {
     entriesManager.entries.push({ id, note });
-    return entriesManager.entries;
+    const allEntries = entriesManager.getAll();
+    return allEntries;
+  },
+
+  edit: (id, note) => {
+    const entry = entriesManager.getOne(id);
+    entry.note = note;
+    return entry;
   }
 };
 
