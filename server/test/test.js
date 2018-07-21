0744e5cfcd7937062a6205f5f90bb37f5f57e598
Ijebusoma <vanessaosuka@gmail.com>
2018-07-21 20:28:34 +0100

    add extra test for modify-entry endpoint

--- server/test/test.js
+++ server/test/test.js
@@ -39,7 +39,7 @@ describe('Get entries', () => {
         done();
       });
   });
-  it('Should get one entry', (done) => {
+  it('It should get one entry', (done) => {
     chai.request(app)
       .get('/api/v1/allentries/1')
       .end((err, res) => {
@@ -52,12 +52,14 @@ describe('Get entries', () => {
   });
 });
 
-describe('Modify entries', () => {
-  it('Should add an entry', (done) => {
+describe('Modify entry', () => {
+  it('It should add an entry', (done) => {
     chai.request(app)
       .post('/api/v1/new/4/ipsum')
       .end((err, res) => {
         expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('message').eql('Successfully added!');
         done();
       });
   });
@@ -66,6 +68,8 @@ describe('Modify entries', () => {
       .put('/api/v1/edit/4/wakanda')
       .end((err, res) => {
         expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        res.body.should.have.property('message').eql('Successfully updated!');
         done();
       });
   });
