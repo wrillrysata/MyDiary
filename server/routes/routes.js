707edcba453d6414107fc6c5ea86c8ac41a093cf
Vanessa Osuka <vanessaosuka@gmail.com>
2018-07-30 00:26:42 +0100

    [Feature #159299828] Implement sign in functionality (#11)
    With this commit, it is possible for users
    to sign in with JWT token authentication.
    
    [#159299828]

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,14 @@
 
+import userController from '../controllers/userController';
+import validation from '../helpers/validation';
+
 const router = (app) => {
   app.get('/', (req, res) => {
     res.status(200)
       .send('Welcome to My Diary App');
   });
+  app.post('/v1/api/auth/signup', validation.userSignupValidation, userController.createUser);
+  app.post('/v1/api/auth/login', userController.loginUser);
 };
 
 
