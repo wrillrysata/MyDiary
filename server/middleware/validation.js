1e56053812042cfab7e2f58a6c9c64aae74cbedc
Ijebusoma <vanessaosuka@gmail.com>
2018-07-31 21:34:59 +0100

    Add email regex
    This commit validates user email
    
    [#159300185]

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -5,9 +5,9 @@ export default{
       userName, email, password, confirmPassword
     } = req.body;
     if (!userName || !email || !password) {
-      return res.json({ message: 'Please fill in all fields'});
+      return res.json({ message: 'Please fill in all fields' });
     }
-    if (typeof email !== 'string') {
+    if (email.trim() === '' || (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email) === false) {
       return res.json({ message: 'Invalid email' });
     }
     if (password !== confirmPassword) {
