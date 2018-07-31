4cb5eafbc6cc46e5f6fe621e2d310b7d628e6bfa
Ijebusoma <vanessaosuka@gmail.com>
2018-07-31 14:00:51 +0100

    Implement feature to prevent multiple signups
    Prior to this commit, a user was able to signup twice
    with same email. This commit checks if a user has an account on the platform
    before signing them up.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,78 +1,94 @@
-/* eslint-disable */
+
 /* https://mherman.org/blog/2015/02/12/postgresql-and-nodejs/ */
-import db from '../helpers/dbHelper';
-import bcrypt from '../helpers/bcrypt';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import db from '../helpers/dbHelper';
+import bcrypt from '../helpers/bcrypt';
 
- dotenv.config()
- const secret = process.env.SECRET_KEY;
+
+dotenv.config();
+const secret = process.env.SECRET_KEY;
 
 export default{
-   createUser: (req, res, next) => {
-            const {
-                userName, email, password, confirmPassword
-            } = req.body;
-            const sql = 'INSERT INTO users ("userName", email, password) values($1, $2, $3) RETURNING *';
-            db.query(sql, [userName, email,bcrypt.hashPassword(password)]),
-            (err, result) => {
-                if (err) {
-                    console.log(err);
-                   return res.status(500).json({
-                    message: 'User registration was not successful, try again please.',
-                   });
-                  
-                } else {
-                    return res.status(201).json({
-                       message: 'User registration successful',
-                       token
-                      });
-                      console.log('inserted');
-        }
-  next();
-    }
-    },
-    loginUser: (req, res) => {
-        const {
-            email, password
-        } = req.body;
-       
-        const sql = 'SELECT * FROM users WHERE email = $1';
-        db.query(sql, [email], (err, result) => {
-            if(err){
-                console.log(err);
-                return res.status(500).json({
-                    message: 'There was an error while signing in user'
-                });
-            }else if (!result){
-                return res.status(404).json({
-                    message: 'No user was found'
-                })
-            }
-            const user = result.rows[0];
-            
-            const rightPassword = bcrypt.comparePassword(password,user.password);
-            if(!rightPassword){
-                return res.status(401).json({
-                    message: 'Password does not match',
-                    token: null,
-                  });
-            }else{
-                const payload = {
-                    name: user.userName 
-                  };
-                  const token = jwt.sign(payload, secret, {
-                    expiresIn: 1440
-                  });
-              return res.status(200).json({
-                message: 'Successfully logged in',
-                token,
-              });
-            
-        }
-    });
-}
-}
+  createUser: (req, res) => {
+    const {
+      userName, email, password,
+    } = req.body;
 
-        
-/* eslint-disable */
+
+    const sql = 'INSERT INTO users ("userName", email, password) values($1, $2, $3) RETURNING *';
+    db.query(
+      sql, [userName, email, bcrypt.hashPassword(password)],
+      (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'User registration was not successful, please try again.',
+          });
+        }
+        return res.status(201).json({
+          message: 'User registration successful',
+        });
+      },
+    );
+  },
+  findUser: (req, res, next) => {
+    const {
+      email,
+    } = req.body;
+    const sql = 'SELECT * FROM users WHERE email = $1';
+    db.query(
+      sql, [email],
+      (err, result) => {
+        if (err) {
+
+          return res.status(500).json({
+            message: 'Could not retrieve User',
+          });
+        }
+        const user = result.rows[0];
+        if (user) {
+          return res.status(409).json({
+            message: 'User already exists',
+          });
+        }
+        next();
+      },
+    );
+  },
+
+  loginUser: (req, res) => {
+    const {
+      email, password,
+    } = req.body;
+
+    const sql = 'SELECT * FROM users WHERE email = $1';
+    db.query(sql, [email], (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          message: 'There was an error while signing in user',
+        });
+      }
+      const user = result.rows[0];
+
+      const rightPassword = bcrypt.comparePassword(password, user.password);
+      if (!rightPassword) {
+        return res.status(401).json({
+          message: 'Password does not match',
+          token: null,
+        });
+      }
+      const payload = {
+        name: user.userName,
+      };
+      const token = jwt.sign(payload, secret, {
+        expiresIn: 1440,
+      });
+      return res.status(200).json({
+        message: 'Successfully logged in',
+        token,
+      });
+    });
+  },
+};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,13 @@
 
 import userController from '../controllers/userController';
-import validation from '../helpers/validation';
+import validation from '../middleware/validation';
 
 const router = (app) => {
   app.get('/', (req, res) => {
     res.status(200)
       .send('Welcome to My Diary App');
   });
-  app.post('/v1/api/auth/signup', validation.userSignupValidation, userController.createUser);
+  app.post('/v1/api/auth/signup', validation.userSignupValidation, userController.findUser, userController.createUser);
   app.post('/v1/api/auth/login', userController.loginUser);
 };
 
