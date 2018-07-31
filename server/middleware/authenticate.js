6d6083e39722b42fecf8d33fa7c4b473861d8120
Ijebusoma <vanessaosuka@gmail.com>
2018-07-31 18:14:22 +0100

    Change status code
    Modify status code
    
    [#159300185]

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,21 +1,18 @@
-/* eslint-disable */
 /* https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens#the-actual-node-application-serverjs */
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
-dotenv.config()
+
+dotenv.config();
 const secret = process.env.SECRET_KEY;
 
 export default {
-  
-  checkToken : (req, res) => {
+  checkToken: (req, res, next) => {
     // check if there's a token
     const token = req.body.token || req.params || req.headers['x-access-token'];
   
-    // decode token
+    // Decode token
     if (token) {
-   // console.log(token);
-   // console.log(' token present');
-      // verifies secret 
+      // Verifies secret 
       jwt.verify(token, secret, function(err, decoded) {      
         if (err) {
           return res.json({ success: false, message: 'Failed to authenticate token.' });    
@@ -26,15 +23,13 @@ export default {
       });
   
     } else {
-  
-      // if there is no token
-      // return an error
-      return res.status(403).send({ 
+      return res.status(401).send({ 
           success: false, 
           message: 'No token provided.' 
       });
   
     }
+    next();
 }
 }
   
