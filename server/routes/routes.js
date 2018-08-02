5352570e4b824acbb9da1aeb8901bad38bbb065c
Vanessa Osuka <vanessaosuka@gmail.com>
2018-08-02 14:14:35 +0100

    Modify route url (#18)

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,15 +9,15 @@ const router = (app) => {
     res.status(200)
       .send('Welcome to My Diary App');
   });
-  app.get('/v1/api/entries', authenticate.checkToken, entryController.getAll);
-  app.get('/v1/api/entries/:id', authenticate.checkToken, entryController.getOne);
-  app.post('/v1/api/entries', authenticate.checkToken, entryController.addNew);
-  app.put('/v1/api/entries/:id', authenticate.checkToken, entryController.updateEntry);
-  app.delete('/v1/api/entries/:id', authenticate.checkToken, entryController.deleteEntry);
-    
+  app.get('/api/v1/entries', authenticate.checkToken, entryController.getAll);
+  app.get('/api/v1/entries/:id', authenticate.checkToken, entryController.getOne);
+  app.post('/api/v1/entries', authenticate.checkToken, entryController.addNew);
+  app.put('/api/v1/entries', authenticate.checkToken, entryController.updateEntry);
+  app.delete('/api/v1/entries/:id', authenticate.checkToken, entryController.deleteEntry);
+
   /** Auth routes */
-  app.post('/v1/api/auth/signup', validation.userSignupValidation, userController.findUser, userController.createUser);
-  app.post('/v1/api/auth/login', userController.loginUser);
+  app.post('/api/v1/auth/signup', validation.userSignupValidation, userController.findUser, userController.createUser);
+  app.post('/api/v1/auth/login', userController.loginUser);
 };
 
 
