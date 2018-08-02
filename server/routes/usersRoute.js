76dc1a7eeec123250b10342fd58135fe81f28e76
Vanessa Osuka <vanessaosuka@gmail.com>
2018-08-02 12:30:12 +0100

    Implement API endpoints (#17)
    This commit implements required crud endpoints
    on API
    
    [Ticket: #159299920]

--- server/routes/usersRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userController from '../controllers/userController';
-import validation from '../helpers/validation';
-
-const usersRouter = express.Router();
-
-usersRouter.post('v1/api/auth/signup', validation.userSignupValidation, userController.createUser);
-usersRouter.post('v1/api/auth/login', validation.userSignupValidation, userController.loginUser);
-export default usersRouter;
