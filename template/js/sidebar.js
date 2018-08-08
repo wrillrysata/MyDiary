e43d30dac193d9f1aad7754d16e2b753a0a44ca7
Vanessa Osuka <vanessaosuka@gmail.com>
2018-08-08 22:07:12 +0100

    [Feature #159554258] Rework user pages (#19)
    There was need to rework user pages and make
    them responsive using Bootstrap framework hence.
    This commit affects templates files to implement necessary changes on the UI.
    
    [Ticket: 159554258]

--- template/js/sidebar.js
+++ template/js/sidebar.js
@@ -2,7 +2,7 @@ const hamburger = document.getElementById('hamburger');
 const close = document.getElementById('close');
 const menu = document.getElementById('Sidenav');
 hamburger.addEventListener('click', function(){
-    menu.style.width = '250px';
+    menu.style.width = '200px';
   })
 close.addEventListener('click', function(){
 menu.style.width = 0;
