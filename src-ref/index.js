bfe25b505df5736b4b89834e44d1cce50b59fd92
Ijebusoma <vanessaosuka@gmail.com>
2019-01-21 13:54:17 +0100

    Remove redundant files

--- src-ref/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
-import style from './css/main.css';
-import cover from './images/cover.jpg';
-
-ReactDOM.render(<App />, document.getElementById('app'));
