caa2838cf261c163f7dd00b9e8ab4b949c7f2513
Ijebusoma <vanessaosuka@gmail.com>
2019-01-21 13:35:00 +0100

    Add necessary files

---
+++ src-ref/Components/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    render() {
+        return (
+            <center>
+    <div class="form col-md-12">
+    <input type="text" value="Enter mail" />
+    <input type="text" value="Username" />
+    <input type="text" value="Enter Password" />
+    <input type="text" value="Confirm Password" />
+    <input type="submit" value="Signup" />
+  </div>
+  </center>
+ )
+}
+}
+export default Signup;
