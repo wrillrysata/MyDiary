89de8f869cc40abae673f5a96e8a67a43cf217a0
Ijebusoma <vanessaosuka@gmail.com>
2019-03-25 11:05:01 +0100

    Delete redundant folder

--- template/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const close = document.getElementById('close');
-const menu = document.getElementById('Sidenav');
-hamburger.addEventListener('click', function(){
-    menu.style.width = '200px';
-  })
-close.addEventListener('click', function(){
-menu.style.width = 0;
-})
