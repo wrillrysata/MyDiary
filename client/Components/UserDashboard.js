4a8ac875ac8ea1ac8fc40261a0dc3f0af14fd44c
Ijebusoma <vanessaosuka@gmail.com>
2019-04-10 11:22:04 +0100

    Minor modifications to app

--- client/Components/UserDashboard.js
+++ client/Components/UserDashboard.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import JwtDecode from 'jwt-decode';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
-import EntryList from './EntryList';
-import { getNotes, saveNote } from '../actions/entryAction';
-import { logoutUser } from '../actions/authAction';
-import Navbar from './HeaderComponents/Navbar'
+import React, { Component } from "react";
+import JwtDecode from "jwt-decode";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
+import EntryList from "./EntryList";
+import { getNotes, saveNote } from "../actions/entryAction";
+import { logoutUser } from "../actions/authAction";
+import Navbar from "./HeaderComponents/Navbar";
 /**
  * @classdesc Sign in users
  */
 class UserDashboard extends Component {
   /**
- * @param {props} props Representing some data passed down
- */
+   * @param {props} props Representing some data passed down
+   */
   constructor(props) {
     super(props);
     this.state = {
-      note: '',
-      message:''
+      note: "",
+      message: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.addNote = this.addNote.bind(this);
@@ -31,7 +31,7 @@ class UserDashboard extends Component {
    */
   onLogoutClick(event) {
     event.preventDefault();
-   
+
     this.props.logoutUser(this.props.history);
   }
 
@@ -41,14 +41,13 @@ class UserDashboard extends Component {
    */
   addNote(event) {
     event.preventDefault();
-    const note  = this.state.note.trim();
-    console.log(note);
-    if(note !== ''){
-    this.props.saveNote(this.state.note, this.props.history);
-    }else{
+    const note = this.state.note.trim();
+    if (note !== "") {
+      this.props.saveNote(this.state.note, this.props.history);
+    } else {
       this.setState({
-        message: 'You have not entered anything bro'
-      })
+        message: "You have not entered any text"
+      });
     }
   }
 
@@ -63,69 +62,66 @@ class UserDashboard extends Component {
     });
   }
 
-  /** Lifecycle method - After component mounts
+  /** Lifecycle method - before component mounts
    * @param {null} null
    * @returns { null} null
    */
-  componentDidMount() {
+  componentWillMount() {
     this.props.getNotes();
-   /**  this.hamburger = document.getElementById('hamburger');
-    this.close = document.getElementById('close');
-    this.menu = document.getElementById('Sidenav');
-    this.close.addEventListener('click', () => {
-      this.menu.style.width = 0;
-    });
-    this.hamburger.addEventListener('click', () => {
-      this.menu.style.width = '200px';
-    });**/
   }
-
-
   /**
- *Renders web page
- * @returns {Webpage} Displays the webpage
- */
+   *Renders web page
+   * @returns {Webpage} Displays the webpage
+   */
   render() {
     return (
-
       <div>
-       <Navbar />
-          <div className="container fluid">
-                <div className="col-md-12">
-                <form onSubmit={ this.addNote } >
-                {this.state.message && <p className="note-text">{this.state.message}</p> }
-                <br></br>
-<textarea placeholder="What's on your mind?" name="note" cols="30" rows="5" onChange={this.handleChange}/>
-<br />
-<input type="submit" value="Add new" ></input>
-                  
-               
-                </form>
-                </div>
+        <Navbar />
+        <div className="container fluid">
+          <div className="col-md-12">
+            <form onSubmit={this.addNote}>
+              {this.state.message && (
+                <p className="error">{this.state.message}</p>
+              )}
+              <br />
+              <textarea
+                placeholder="What's on your mind like right now?"
+                name="note"
+                cols="30"
+                rows="5"
+                onChange={this.handleChange}
+              />
+              <br />
+              <input type="submit" value="Add note" />
+            </form>
+          </div>
+        </div>
 
-
-                 </div>
-
-                 <center><h3 className="clearfix">My Notes</h3></center>
-                 <hr />
-                 <div className="container fluid">
-                 {this.props.entries.entries.map(entry => (
-<EntryList entry={ entry } key={entry.id } />
-                 ))}
-
-            </div>
-
+        <center>
+          <h3 className="clearfix">My Notes</h3>
+        </center>
+        <hr />
+        <div className="container fluid">
+        {this.props.entries.entries.length > 0 ?
+          this.props.entries.entries.map(entry => (
+            <EntryList entry={entry} key={entry.id} />
+          )):<div className="well"><center><p className="note-text">You have no notes, write something to get started</p></center></div>
+        }   
+        </div>
+        <p className="footer footer-text">Made with <i className="fas fa-heart"></i> by <a href="https://github.com/Ijebusoma">Vanessa</a></p>
       </div>
+     
+          
     );
   }
 }
+
 UserDashboard.propTypes = {
   getNotes: PropTypes.func.isRequired,
   saveNote: PropTypes.func.isRequired,
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
-
 };
 const mapStateToProps = state => ({
   auth: state.auth,
@@ -133,5 +129,7 @@ const mapStateToProps = state => ({
   entries: state.entries
 });
 
-export default connect(mapStateToProps,
-  { logoutUser, getNotes, saveNote })(withRouter(UserDashboard));
+export default connect(
+  mapStateToProps,
+  { logoutUser, getNotes, saveNote }
+)(withRouter(UserDashboard));
